ca5cd83b6e75d31138bba52538136df42e9fd0c3
Ross Cooper <ross@craftaweb.co.uk>
2020-10-15 20:51:24 +0100

    Updated packages and removed node-sass-middleare

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var sassMiddleware = require('node-sass-middleware');
 var bodyParser     =        require("body-parser");
 
 var indexRouter = require('./routes/index');
@@ -20,12 +19,6 @@ app.use(bodyParser.json({ limit: '100mb'}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(sassMiddleware({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: true, // true = .sass and false = .scss
-  sourceMap: true
-}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
